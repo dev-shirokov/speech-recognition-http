48c988589d5b5a9b980b28de54b3a8fbd0ec3962
Anatoly Shirokov <dev.shirokov@gmail.com>
2023-03-12 18:41:20 +0300

    docker-compose

---
+++ api/Controllers/ServiceEndpointsOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace api.Controllers;
+
+public class ServiceEndpointsOptions
+{
+    public string VoskKaldiRu { get; set; }
+    public static string Position => "ServiceEndpoints";
+}
---
+++ api/Program.cs
@@ -0,0 +1,28 @@
+using api.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ServiceEndpointsOptions>(builder.Configuration.GetSection(ServiceEndpointsOptions.Position));
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
